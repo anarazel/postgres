572e8976ac2df8ac8eae880cd66104be438830b0
Tomas Vondra <tomas@vondra.me>
2026-02-17 15:06:34 +0100

    [skip ci] Reapply "explain: show prefetch stats for indexes"
    This reverts commit ee279b29b5175536426eac06bbcff82077c2d88b.

--- src/backend/access/heap/heapam_handler.c
+++ src/backend/access/heap/heapam_handler.c
@@ -1070,6 +1070,7 @@ heapam_getnext_stream(ReadStream *stream, void *callback_private_data,
 			(prefetchBatch->visInfo[prefetchPos->item] & BATCH_VIS_ALL_VISIBLE))
 		{
 			/* item is known to be all-visible -- don't prefetch */
+			read_stream_skip_block(stream);
 			continue;
 		}
 
@@ -1083,6 +1084,7 @@ heapam_getnext_stream(ReadStream *stream, void *callback_private_data,
 			 * block number; we must not return the same prefetch_block twice
 			 * (twice in succession)
 			 */
+			read_stream_skip_block(stream);
 			continue;
 		}
 
